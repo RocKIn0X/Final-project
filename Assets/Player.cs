3c8b2dc432e27bc94bc1eb44b112f3468ee2be65
seaiin <namefinal@gmail.com>
2019-02-05 18:14:06 +0700

    Add coin UI and Player Object

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI coinText;
+    public int coinAmount = 0;
+
+    private void Start()
+    {
+        SetCointText();
+    }
+
+    public void AddCoin(int amount)
+    {
+        coinAmount += amount;
+        SetCointText();
+    }
+
+    private void SetCointText()
+    {
+        coinText.text = "coin : " + coinAmount.ToString();
+    }
+}
