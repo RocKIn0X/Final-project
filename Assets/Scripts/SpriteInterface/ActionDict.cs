05fdf0d037d75819a149815660b1b163d874268f
Suphanath Kaewboonruang <suphanathk@keemail.me>
2019-03-19 11:29:12 +0700

    Add action bubble script

---
+++ Assets/Scripts/SpriteInterface/ActionDict.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionDict : MonoBehaviour
+{
+    [System.Serializable]
+    public struct ActionDictionary {
+        public string actionKey;
+        public Sprite actionTarget;
+    } ;
+
+    public List<ActionDictionary> ActionBook = new List <ActionDictionary> ();
+
+    private static Dictionary<string, Sprite> ActualDict = new Dictionary <string, Sprite> ();
+
+    void PopulateDict()
+    {
+        foreach (ActionDictionary value in ActionBook) {
+            if (value.actionTarget != null) {
+                ActualDict.Add(value.actionKey, value.actionTarget);
+            }
+        }
+    }
+
+    public static Sprite GetActionSprite(string key)
+    {
+        return ActionDict.ActualDict[key];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateDict();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
