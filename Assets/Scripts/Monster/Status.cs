64e7400b0949f40073673356caf9ae263e6a64cd
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-03-19 15:04:50 +0700

    Add status script

---
+++ Assets/Scripts/Monster/Status.cs
@@ -0,0 +1,14 @@
+﻿[System.Serializable]
+public class Status
+{
+    public int hungry;
+    public int tireness;
+    public int emotion;
+
+    public Status ()
+    {
+        hungry = 100;
+        tireness = 100;
+        emotion = 100;
+    }
+}
