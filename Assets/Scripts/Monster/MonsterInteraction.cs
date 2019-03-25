760f59c24295d722f42f18d722feb60bf38c7fc1
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-03-25 22:57:24 +0700

    Add monster interaction script

---
+++ Assets/Scripts/Monster/MonsterInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class MonsterInteraction : MonoBehaviour
+{
+    public GameObject tileTarget;
+    public ActionBubble actionBubble;
+
+    [SerializeField]
+    private Status status;
+    [SerializeField]
+    private float timePerAction;
+
+    private void Start()
+    {
+        status = new Status();
+    }
+
+    public void DisplayBubble (int index)
+    {
+        // actionBubble.ShowAction(timePerAction);
+    }
+
+    public void MonsterAction (int index)
+    {
+        // tileTarget.GetComponent<TileClass>().Action(index, this);
+        // DisplayBubble(index);
+    }
+
+    public void SetStatus (int hungry, int tireness, int emotion)
+    {
+        status.SetStatus(hungry, tireness, emotion);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        tileTarget = col.gameObject;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject == tileTarget)
+        {
+            tileTarget = null;
+        }
+    }
+}
