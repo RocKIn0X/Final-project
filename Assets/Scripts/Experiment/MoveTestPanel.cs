674cd03681687356aab291ef9791b7bc057ec0fe
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-04-04 15:37:36 +0700

    slide the slider to set reward

--- Assets/Scripts/Experiment/MoveTestPanel.cs
+++ Assets/Scripts/Experiment/MoveTestPanel.cs
@@ -9,20 +9,52 @@ public class MoveTestPanel : MonoBehaviour
     public InputField hungerField;
     public InputField tirenessField;
     public InputField emotionField;
+    public Slider rewardSlider;
     public TextMeshProUGUI actionText;
 
     private Brain brain;
 
+    private bool isAutoTest = false;
+    private float reward;
+
     // Start is called before the first frame update
     void Start()
     {
+        reward = 0f;
+
         brain = GetComponent<Brain>();
+
+        rewardSlider.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RunAutoTest ()
+    {
+        if (!isAutoTest)
+        {
+            StartCoroutine(AutoTest());
+            isAutoTest = true;
+        }
+    }
+
+    public IEnumerator AutoTest ()
+    {
+        CalculateAction();
+        // activate reward
+        rewardSlider.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+
+        // deactivate reward
+        rewardSlider.gameObject.SetActive(false);
+        // set reward to ANN
+        Debug.Log("Reward: " + reward);
+        // end auto test
+        isAutoTest = false;
     }
 
     public void SetStates (out List<double> states)
@@ -49,5 +81,10 @@ public class MoveTestPanel : MonoBehaviour
         if (maxQIndex == 0) actionText.text = "Working";
         else if (maxQIndex == 1) actionText.text = "Eatting";
         else if (maxQIndex == 2) actionText.text = "Sleeping";
+    }
+
+    public void AdjustReward ()
+    {
+        reward = rewardSlider.value;
     }
 }
