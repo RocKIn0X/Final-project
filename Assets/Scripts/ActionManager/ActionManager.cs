2100ad07eedd7f61402a38133c3daa1cfc3378ec
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2019-04-15 12:09:58 +0700

    Create brain collection to keep brain of each state

--- Assets/Scripts/ActionManager/ActionManager.cs
+++ Assets/Scripts/ActionManager/ActionManager.cs
@@ -2,10 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class BrainCollection
+{
+    public string nameState;
+
+    // ANN
+    [Header("[ANN parameters]")]
+    public int numInputs;
+    public int numOutputs;
+    public List<int> numNEachLayer;
+    public double alpha;
+
+    // Reinforcement
+    [Header("[Reinforcement parameters]")]
+    public float discount;
+
+    private MonsterBrain brain;
+
+    public void InitEachBrain()
+    {
+        brain = new MonsterBrain(numInputs, numOutputs, numNEachLayer, alpha, discount);
+    }
+
+    public int CalculateAction(List<double> states)
+    {
+        return brain.CalculateAction(states);
+    }
+
+    public List<double> GetQS()
+    {
+        return brain.GetQS();
+    }
+
+    public void SetMemory(List<double> states, float reward)
+    {
+        brain.SetMemory(states, reward);
+    }
+}
+
 public class ActionManager : MonoBehaviour
 {
-    [SerializeField]
-    MonsterBrain moveBrain;
+    public List<BrainCollection> brainCollections = new List<BrainCollection>();
+
+    #region Reward
+    public float idleReward;
+    public float praiseReward;
+    public float punishReward;
+    #endregion
 
     //MonsterBrain actionBrain;
 
@@ -30,30 +74,36 @@ public class ActionManager : MonoBehaviour
         moveHiddenLayer.Add(4);
         moveHiddenLayer.Add(6);
         moveHiddenLayer.Add(4);
-        moveBrain = new MonsterBrain(3, 3, moveHiddenLayer, 0.2f);
+        //moveBrain = new MonsterBrain(3, 3, moveHiddenLayer, 0.2f);
+        //moveBrain = new MonsterBrain();
 
         /*
         List<int> actionHiddenLayer = new List<int>();
         actionHiddenLayer.Add(3);
         actionBrain = new MonsterBrain(2, 3, actionHiddenLayer, 0.2f);
         */
+
+        foreach (var b in brainCollections)
+        {
+            b.InitEachBrain();
+        }
     }
 
     public int CalculateAction (List<double> states)
     {
-        return moveBrain.CalculateAction(states);
+        return brainCollections[0].CalculateAction(states);
     }
 
     public List<double> GetQS ()
     {
-        return moveBrain.GetQS();
+        return brainCollections[0].GetQS();
 
         //return actionBrain.GetQS();
     }
 
     public void SetMemory (List<double> states, float reward)
     {
-        moveBrain.SetMemory(states, reward);
+        brainCollections[0].SetMemory(states, reward);
         //actionBrain.SetMemory(states, reward);
     }
 }
