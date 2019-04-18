6aa9f9ca6a50fd88ec1e2031ff5d9e1f59f0e4dc
Suphanath Kaewboonruang <suphanathk@keemail.me>
2019-04-18 16:19:03 +0700

    Change visualiser, and add HighscoreText

--- Assets/Scripts/UI/Visualiser/ChartObject.cs
+++ Assets/Scripts/UI/Visualiser/ChartObject.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChartObject : MonoBehaviour
 {
     public GameObject chartRoot;
+    public TextMeshProUGUI scoreText;
     public ChartBar chartBarPrefab;
 
     private Dictionary<int, float> chartData = new Dictionary<int, float>();
 
     public float chartMaxHeight;
     private float dataMaxValue;
+
+    public float visualMaxProfit = 0;
+    public float visualMaxDeficit = 0;
+    public float latestProfit = 0;
 
     public Color barColorPositive;
     public Color barColorNegative;
@@ -23,14 +29,30 @@ public class ChartObject : MonoBehaviour
         return bar;
     }
 
+    public void UpdateText()
+    {
+        scoreText.text = "This Month Profit:\t" + latestProfit +
+            "\nHighest Profit:\t" + visualMaxProfit +
+            "\nHighest Deficit:\t" + visualMaxDeficit;
+    }
 
     public void AddValue(int key, float value)
     {
+        latestProfit = value;
         chartData[key] = value;
+        if (value > visualMaxProfit)
+        {
+            visualMaxProfit = value;
+        }
+        if (value < visualMaxDeficit)
+        {
+            visualMaxDeficit = value;
+        }
         if (Mathf.Abs(value) > dataMaxValue)
             dataMaxValue = Mathf.Abs(value);
         AddChartBar(value);
         UpdateChart();
+        UpdateText();
     }
 
     public void UpdateChart()
@@ -43,9 +65,15 @@ public class ChartObject : MonoBehaviour
                 float calculatedHeight = chartMaxHeight * (Mathf.Abs(currentBar.barValue) / dataMaxValue);
                 currentBar.SetHeight(calculatedHeight);
                 if (currentBar.barValue < 0)
+                {
                     currentBar.SetColor(barColorNegative);
+                    currentBar.SetPositive(false);
+                }
                 else
+                {
                     currentBar.SetColor(barColorPositive);
+                    currentBar.SetPositive(true);
+                }
             }
         }
     }
@@ -55,7 +83,7 @@ public class ChartObject : MonoBehaviour
         int MAX_TEST = 100;
         for (int index = 0 ; index < MAX_TEST ; index = index + 1)
         {
-            AddValue(index, Random.Range(-255f, 255f));
+            AddValue(index, Random.Range(-256f, 512f));
         }
     }
 
