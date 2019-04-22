9af63ab54b11291e59384fa1bd918e9a5c514ada
Suphanath Kaewboonruang <suphanathk@keemail.me>
2019-04-22 19:26:21 +0700

    Fix TrainingPopup

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -119,15 +119,6 @@ public class PlayerManager : MonoBehaviour
     }
     private void InputProcess()
     {
-
-        if (popup == null)
-            popup = (TrainningPopup)FindObjectOfType(typeof(TrainningPopup));
-        if (popup.trainable == false)
-        {
-            Debug.Log("Not Trainable");
-            return;
-        }
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         LayerMask layerMask = LayerMask.GetMask("Monster");
         RaycastHit[] hits = Physics.RaycastAll(ray, 1000, layerMask);
